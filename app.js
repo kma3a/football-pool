5e499a6656e57612a2de68a327c79d145377583e
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-04 17:13:29 -0400

    adding the code for login and setup for all of that

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -22,10 +25,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({
+  secret: 'mySecretKey', 
+  proxy: true, 
+  resave: true, 
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
 app.use('/tickets', tickets);
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
+  })
+);
+
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+
+app.get('/loginSuccess', function(req, res, next) {
+  res.send('Successfully authenticated');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(function(email, password, done) {
+  User.find({where: {email: email}}).success(function(user) {
+    if(!user || !isValidPassword(user, password)) {
+      done(null, false, {message: 'Invalid username or password'});
+    } else {
+      done(null, user);
+    }
+  }).error(function(err) {
+      done(err);
+    });
+  }
+));
+
+var isValidPassword = function(user, password) {
+  return bCrypt.compareSync(password, user.password);
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
