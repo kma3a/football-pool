1caf32facda5d16fcfe9c48e6a5c28ef29367ed4
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-04 17:12:39 -0400

    adidng routes for the login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.post('/login', passport.authenticate('login', {
+  successRedirect: '/user',
+  faulureRedirect: '/login',
+  failureFlash: true
+}));
 
 module.exports = router;
