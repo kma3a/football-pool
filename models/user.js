93620955c62d79c1f06f52c37024c87e91fcc3ae
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-04 16:18:50 -0400

    adding a user to the model for testing purposes

--- models/user.js
+++ models/user.js
@@ -3,8 +3,11 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: DataTypes.String,
+    email: DataTypes.String,
     password: DataTypes.String,
     admin   : DataTypes.Boolean
   });
 
 User.sync();
+
+var user = User.create({username: "helloWorld1", email: "d.chan42@gmail.com", password: "Test1234", admin: true});
