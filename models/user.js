0fcb83af966dce725ae3ffb8f8df34f8d18184a6
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-04 23:03:23 -0400

    changing some of the code to hopefully get it running

--- models/user.js
+++ models/user.js
@@ -2,12 +2,13 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    username: DataTypes.String,
-    email: DataTypes.String,
-    password: DataTypes.String,
-    admin   : DataTypes.Boolean
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    admin   : DataTypes.BOOLEAN
   });
 
 User.sync();
 
 var user = User.create({username: "helloWorld1", email: "d.chan42@gmail.com", password: "Test1234", admin: true});
+}
