e3e030d0a4664ae1f5ae80ee43eaca2a9eefae7a
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-10 12:52:08 -0400

    adding code to route to the passport for login

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
-router.post('/login', passport.authenticate('login', {
+router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/user',
   faulureRedirect: '/login',
   failureFlash: true
