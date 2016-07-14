2a7cad4d23d76a6681d83b3cc22b11dd89bfd1f4
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-14 00:07:09 -0400

    adding the code to make the admin view work

--- routes/index.js
+++ routes/index.js
@@ -51,25 +51,20 @@ router.get('/user/:username/edit', isLoggedIn, function(req, res, next) {
 router.post('/user/:username/update', isLoggedIn, function(req, res, next) {
   var user = req.user;
   var params = req.body;
-  console.log("I AM THE PARAMS", params);
 
   if(!user.validPassword(params.old_password)) {
-    console.log("not valid password");
     res.redirect('/user/'+user.username+'/edit');
   }
 
   if(!checkPassword(params.new_password, params.new_password_confirm)) {
-    console.log("not valid passwords");
     res.redirect('/user/'+user.username+'/edit');
   }
 
   if(!validateEmail(params.email)) {
-    console.log("not valid email");
     res.redirect('/user/'+user.username+'/edit');
   }
 
   user.update({email: params.email, password: params.new_password}, {where: {username: user.username}}). then(function(currentUser) {
-    console.log("I AM THE USER", currentUser);
     res.redirect('/user/'+user.username);
     
   }, function (err) {
@@ -87,13 +82,16 @@ router.post('/user/:username/update', isLoggedIn, function(req, res, next) {
 //also not working but committing for now
 router.get('/admin/:username',function(req, res, next) {
   var user = req.user;
-  User.findAll().then(function(userlist) {
-    if(userlist) {
-      res.render('admin', {user: user, userlist: userlist});
+  User.all().then(function(userlist) {
+    if(userlist && userlist.length > 0) {
+      res.render('admin', { user: user, userlist: userlist});
     } else {
       res.redirect("/user/"+ user.username);
     }
-  }, function(err) {console.log("I errored", err)});
+  }, function(err) {
+      console.log("I errored", err);
+      res.redirect("/user/"+ user.username);
+  });
 });
 
 
@@ -109,10 +107,12 @@ router.param('username', function(req, res, next, username) {
 });
 
 
+//TODO make this part of it's own module so we can use it more easily in other areas or part of the user module
 function checkPassword(password, password_confirm) {
   return password === password_confirm;
 }
 
+//TODO make this part of the user model
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
