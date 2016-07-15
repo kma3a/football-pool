f767a787611618cb172e7bb1fe1e6a55b3b6f0bc
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-14 22:14:58 -0400

    changing how we are doing the model incase they don't want to update the password

--- models/user.js
+++ models/user.js
@@ -13,12 +13,15 @@ module.exports = function(sequelize, DataTypes) {
       return user.password = createPasswordHash(user.password);
     },
     beforeUpdate: function(user, options) {
+      console.log("password", user.password);
+      console.log("password", options);
+      if (options.skip.indexOf('password') > -1) {return; }
       return user.password = createPasswordHash(user.password);
     }
   },
   instanceMethods: {
     validPassword: function(password) {
-      console.log("I have password:", this.password);
+      console.log("compare", bcrypt.compareSync(password, this.password));
       return bcrypt.compareSync(password, this.password);
     }
    }
