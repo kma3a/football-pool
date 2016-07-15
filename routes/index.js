f07336841ea2a57f9baac0f7f1dbbcaaf1e3a736
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-14 21:26:58 -0400

    removing the user paths from index and making it so the nodemailer isn't in there too

--- routes/index.js
+++ routes/index.js
@@ -37,48 +37,6 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-router.get('/user/:username', isLoggedIn, function(req, res, next) {
-  var user = req.user;
-  res.render('profile', { title: user.username + "'s Profile", user: user});
-});
-
-router.get('/user/:username/edit', isLoggedIn, function(req, res, next) {
-  var user = req.user;
-  res.render('editProfile', { title: 'Edit ' + user.username + "'s Profile", user: user});
-});
-
-//still working on this route it is currently broken
-router.post('/user/:username/update', isLoggedIn, function(req, res, next) {
-  var user = req.user;
-  var params = req.body;
-
-  if(!user.validPassword(params.old_password)) {
-    res.redirect('/user/'+user.username+'/edit');
-  }
-
-  if(!checkPassword(params.new_password, params.new_password_confirm)) {
-    res.redirect('/user/'+user.username+'/edit');
-  }
-
-  if(!validateEmail(params.email)) {
-    res.redirect('/user/'+user.username+'/edit');
-  }
-
-  user.update({email: params.email, password: params.new_password}, {where: {username: user.username}}). then(function(currentUser) {
-    res.redirect('/user/'+user.username);
-    
-  }, function (err) {
-    res.redirect('/user/'+user.username+'/edit');
-  })
-
-  /*
-
-  console.log("not valid other");
-  res.render('profile', { title: user.username + "'s Profile", user: user});
-  */
-
-});
-
 //also not working but committing for now
 router.get('/admin/:username',function(req, res, next) {
   var user = req.user;
@@ -105,26 +63,6 @@ router.param('username', function(req, res, next, username) {
     next()
   }, function (err) {next(err)});
 });
-
-
-//TODO make this part of it's own module so we can use it more easily in other areas or part of the user module
-function checkPassword(password, password_confirm) {
-  return password === password_confirm;
-}
-
-//TODO make this part of the user model
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-
-function isLoggedIn(req, res, next) {
-  if (req.session.isLoggedIn){
-    return next();
-  } else {
-    res.redirect('/login');
-  }
-}
 
 function isAdmin(req, res, next) {
   console.log("I AM THE USER", req.user);
