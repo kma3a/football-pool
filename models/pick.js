90b87c6cbbe31e0cedc5fe63b32df068a8ad4c70
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-25 23:49:49 -0400

    adding has won to the model of the picks

--- models/pick.js
+++ models/pick.js
@@ -4,6 +4,7 @@ module.exports = function(sequelize, DataTypes) {
   var Pick = sequelize.define("Pick", {
     teamChoice: DataTypes.STRING,
     active:     DataTypes.BOOLEAN,
+    hasWon:     DataTypes.BOOLEAN,
     week:       DataTypes.INTEGER,
     hasPaid:    DataTypes.BOOLEAN
   });
