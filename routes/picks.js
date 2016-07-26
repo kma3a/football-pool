96dbd9edb1fe217f0c202f7e547308a9b99c4c6d
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-26 18:27:30 -0400

    adding what I have so far for the inital picks

--- routes/picks.js
+++ routes/picks.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var CONSTANT = require('../config/constant');
+var Game = require('../models/index.js').Game;
+var Pick = require('../models/index.js').Pick;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/new', function(req, res, next) {
+  var user = req.user;
+  res.render('picks', {user: user, teams: CONSTANT.teams});
 });
+
+router.post('/new', function(req, res, next) {
+  var user = req.user;
+  Game.findOne({where: {inProgress: true, loserGame: false}})
+    .then();
+});
+
 
 module.exports = router;
