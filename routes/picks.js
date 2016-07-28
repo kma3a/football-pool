935b2d365dd7741ca75f355e956f5581b4a6bd6f
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-07-27 20:12:58 -0400

    adding what I have so far

--- routes/picks.js
+++ routes/picks.js
@@ -7,7 +7,7 @@ var Pick = require('../models/index.js').Pick;
 /* GET users listing. */
 router.get('/new', isLoggedIn, function(req, res, next) {
   var user = req.user;
-  res.render('newPicks', {user: user, teams: CONSTANT.teams});
+  res.render('newPicks', {title: "choose your starting team", user: user, teams: CONSTANT.teams});
 });
 
 router.post('/new',isLoggedIn, function(req, res, next) {
@@ -32,9 +32,37 @@ router.post('/new',isLoggedIn, function(req, res, next) {
       }
     }, failure);;
   }
+
   function failure(err) {
     res.redirect("/picks/new");
   };
+
+});
+
+router.get('/:pickId', isLoggedIn, function(req, res, next) {
+  var pick = req.params.pickId;
+  Pick.findOne({where: {id: pick}}).then(
+    function(pick) {
+      res.render('picks', {title: "choose Next Weeks Team", user: user, teams: CONSTANT.teams, pick: pick});
+    }, failure);
+});
+
+router.post('/:pickId', isLoggedIn, function(req, res, next) {
+  var pick = req.params.pickId;
+  Pick.findOne({where: {id: pick}}).then(
+    function(pick) {
+      Pick.create({active: true, hasWon: false, week: pick.week + 1, hasPaid: pick.hasPaid, teamChoice: req.body.teamPick})
+      .then(function(newPick) {
+        newPick.setUser(user);
+        newPick.setGame(pick.getGame);
+        newPick.save();
+        res.redirect('/');
+    }, failure);
+
+  function failure(err) {
+    res.redirect("/picks/"+ pick);
+  };
+
 });
 
 function isLoggedIn(req, res, next) {
