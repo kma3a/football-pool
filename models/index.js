4fbdd47f980cda6b298460a14b64701ef3b632f4
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-08-10 22:08:02 -0400

    adding changes to hopefully connect the db

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
+var pg        = require('pg');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.js')[env];
@@ -11,7 +12,9 @@ var db        = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  pg.connect(process.env.DATABASE_URL, function() {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  });
 }
 
 
