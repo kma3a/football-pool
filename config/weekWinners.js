10cb5850e60410797d010c5840b3355c15958759
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-08-25 21:47:19 -0400

    putting the week winners code in its own file

---
+++ config/weekWinners.js
@@ -0,0 +1,56 @@
+var Pick = require('./models/index.js').Pick;
+var currentGame= require("./config/game");
+var currentLoserGame = require("./config/loserGame");
+var playingTeams = require("./config/getPlayingTeams");
+
+function weeklyPickUpdate() {
+  var date = new Date();
+  date.setDate(date.getDate() - 7 )
+  playingTeams.getFirstWeeks(2016)
+    .then(playingTeams.getGamesOnDate.bind(null, date, true, true))
+    .then(updateWeek)
+    .then(playingTeams.getGamesOnDate.bind(null, new Date(), true))
+    .then(updateGames);
+}
+
+function updateGames(games) {
+  currentGame.init()
+    .then(currentLoserGame.init)
+    .then(changeGameData);
+  function changeGameData(){
+    currentGame.get().update({weekGames: games.data});
+    if(currentLoserGame.get()) {
+      currentLoserGame.get().update({weekGames: games.data});
+    }
+  }
+}
+
+// this function will update the information for the week.
+function updateWeek(winningTeams) {
+  var game = currentGame.get();
+  var loserGame = currentLoserGame.get();
+  Pick.findAll({where: {GameId: game.id, week: game.weekNumber}})
+    .then(function(gamePicks) {
+        updatePicks(gamePicks)
+        game.update({weekNumber: game.weekNumber +1, canEdit: true});
+    });
+  
+  if(loserGame) {
+    Pick.findAll({where: {GameId: loserGame.id, week: loserGame.weekNumber}})
+      .then( function(gamePicks) {
+        updatePicks(gamePicks)
+        
+        loserGame.update({weekNumber: loserGame.weekNumber +1, canEdit: true});
+      });
+  }
+
+  function updatePicks(gamePicks){
+    gamePicks.forEach(function(pick){
+     if(winningTeams.data.indexOf(pick.teamChoice) >=0) {
+       pick.update({hasWon: true});
+      }
+    })
+  }
+  return Promise.resolve();
+}
+
