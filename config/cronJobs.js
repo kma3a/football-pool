8e41829d9fb15eaeb976a7aa0877ad8f870b892e
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-08-26 18:36:59 -0400

    changing how we are doing the jobs

--- config/cronJobs.js
+++ config/cronJobs.js
@@ -1,22 +1,14 @@
-var CronJob = require('cron').CronJob;
 var job2 = require('../config/weekWinners');
 var job3 = require('../config/fridayPick');
 
 console.log("I am doign things", job2, job3);
+var date = new Date().getDay();
+if( date === 6){
+  //cron job for checking the picks and adding any of the 
+    job3.start()
+} else if (date === 2) {
 //cron job to update the picks to put in who is winning for the week and the weekNumber for the game.
+    job2.start()
+}
 
-var job2 = new CronJob({
-    cronTime: '00 15 00 * * 2',
-    onTick: job2.start,
-    start: false
-});
-job2.start();
-
-//cron job for checking the picks and adding any of the 
-var job3 = new CronJob({
-    cronTime: '00 10 00 * * 6',
-    onTick: job3.start,
-    start: false
-});
-job3.start();
 
