7c7fa413229a8dc31c7234014748c6a7f08240f7
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-09-15 23:26:38 -0400

    adding code to do a quick run

--- bin/cronJobs.js
+++ bin/cronJobs.js
@@ -7,7 +7,7 @@ var date = new Date().getDay();
 if( date === 6){
   //cron job for checking the picks and adding any of the 
     job3.start()
-} else if (date === 2) {
+} else if (date === 4) {
 //cron job to update the picks to put in who is winning for the week and the weekNumber for the game.
     job2.start()
 } else if (date === 5) {
