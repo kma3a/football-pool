0098f53dc9c712eb095ca7902c9d0c8a9506f6f7
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-09-15 23:33:56 -0400

    adding changes to make it work for testing on heroku:

--- bin/cronJobs.js
+++ bin/cronJobs.js
@@ -7,10 +7,10 @@ var date = new Date().getDay();
 if( date === 6){
   //cron job for checking the picks and adding any of the 
     job3.start()
-} else if (date === 4) {
+} else if (date === 5) {
 //cron job to update the picks to put in who is winning for the week and the weekNumber for the game.
     job2.start()
-} else if (date === 5) {
+} else if (date === 4) {
 //cron job to update the picks to put in who is winning for the week and the weekNumber for the game.
     job1.start()
 }
