acd08b166cde2edb30cb45b05ce8c737a150c9cb
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-09-18 20:01:09 -0400

    fixing the tabs on the bin/cronjobs and adding the code for the weekwinner back in

--- bin/cronJobs.js
+++ bin/cronJobs.js
@@ -6,14 +6,13 @@ var job3 = require('../config/fridayPick');
 var date = new Date().getDay();
 if( date === 6){
   //cron job for checking the picks and adding any of the 
-    job3.start()
+  job3.start()
 } else if (date === 2) {
 //cron job to update the picks to put in who is winning for the week and the weekNumber for the game.
-//commenting out this week because I ran the job early
-    //job2.start()
+  job2.start()
 } else if (date === 5) {
 //cron job to update the picks to put in who is winning for the week and the weekNumber for the game.
-    job1.start()
+  job1.start()
 }
 
 
